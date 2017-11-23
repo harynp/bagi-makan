9e8f6df012833be4c96f946692c76362ced6e35f
Hary Nugraha Putra <harynugrahaputra@gmail.com>
2017-11-23 18:06:26 +0700

    Success Upload Multer

--- server/routes/foodRoute.js
+++ server/routes/foodRoute.js
@@ -1,10 +1,24 @@
 const route = require('express').Router()
 const Food = require('../controllers/foodController')
+var images = require('../helpers/images')
 
 route.get('/', Food.findAllFood)
 route.get('/:id', Food.findFoodById)
-route.post('/', Food.createFood)
+// route.post('/', Food.createFood)
+
+route.post('/',
+  images.multer.single('imgUrl'),
+  images.sendUploadToGCS,
+  (req, res) => {
+    res.send({
+      status: 200,
+      message: 'Your file is successfully uploaded',
+      link: req.file.cloudStoragePublicUrl
+    })
+  }, Food.createFood)
+
 route.put('/:id', Food.editFoodById)
 route.delete('/:id', Food.deleteFoodById)
+
 
 module.exports = route
