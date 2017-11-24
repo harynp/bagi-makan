6e2c7125578ed1fc86a41f70cf88ea8ede679f25
Hary Nugraha Putra <harynugrahaputra@gmail.com>
2017-11-24 14:00:25 +0700

    On Proggress Fix

--- client/assets/facebook.js
+++ client/assets/facebook.js
@@ -1,10 +1,9 @@
 function statusChangeCallback(response) {
   console.log('TES',response);
     if (response.status === 'connected') {
-      axios.post('http://localhost:3000/facebook',{headers: {token:response.authResponse.accessToken , id: response.authResponse.userID}})
+      axios.post('http://localhost:3000/facebook',{headers: { token:response.authResponse.accessToken , id: response.authResponse.userID}})
       .then(function (rsp) {
         console.log('~~~~ RSP ', rsp);
-
         localStorage.setItem('jwtTokenApp',rsp.data.token)
         localStorage.setItem('name', rsp.data.name)
         window.location.href = 'index.html'
