a0672bfb89fa589253496583d713e43c33f4d63e
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-24 13:46:19 +0700

    Client add image upload from client and fix layout

--- client/assets/facebook.js
+++ client/assets/facebook.js
@@ -3,6 +3,8 @@ function statusChangeCallback(response) {
     if (response.status === 'connected') {
       axios.post('http://localhost:3000/facebook',{headers: {token:response.authResponse.accessToken , id: response.authResponse.userID}})
       .then(function (rsp) {
+        console.log('~~~~ RSP ', rsp);
+
         localStorage.setItem('jwtTokenApp',rsp.data.token)
         localStorage.setItem('name', rsp.data.name)
         window.location.href = 'index.html'
--- client/js/components/food-item.js
+++ client/js/components/food-item.js
@@ -2,15 +2,15 @@ var foodItem = Vue.component('food-item', {
 	template: `
 	<div class="food-item">
 		<figure class="food-pic">
-			<img v-bind:src="photo.urls.regular" alt="food" />
+			<img v-bind:src="photo.imgUrl" alt="food" />
 
 			<figcaption class="pic-detail">
-				<h3 class="pic-title">{{ photo.user.name }} @ {{ photo.user.first_name }}</h3>
+				<h3 class="pic-title">{{ photo.title }} @ {{ photo.location }}</h3>
 
 				<div class="show-hover">
 					<h4 class="pic-owner text-center">By <a href="#">Hary</a></h4>
 
-					<a v-bind:href="photo.urls.regular" v-bind:download="photo.urls.regular" class="btn btn-warning u-full-width">Download</a>
+					<a v-bind:href="photo.imgUrl" v-bind:download="photo.imgUrl" class="btn btn-warning u-full-width">Download</a>
 				</div>
 			</figcaption>
 		</figure>
